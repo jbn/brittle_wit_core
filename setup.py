05729f5459969fee385dd4d7b440c3f227bfc225
John B Nelson <jbn@artifexdeus.com>
2017-09-01 18:17:03 -0700

    Update .gitignore and shield artifacts

--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@ CLASSIFIERS = [
     "Natural Language :: English",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
